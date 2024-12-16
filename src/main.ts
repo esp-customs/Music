0d03580814843099745af0e90b850c9d0f40786d
SrGobi <alejandrosanjuangallegos@gmail.com>
2024-12-16 15:29:39 +0100

    feat: Renombrar Player a MusicPlayer en la exportación de main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
 export { MusicClient } from './music/client';
-export { Player, PlayerOptions, Track } from './music/player';
+export { MusicPlayer, PlayerOptions, Track } from './music/player';
 export * as Q from './music/q';
 export { MusicClientEvent } from './music/events';
