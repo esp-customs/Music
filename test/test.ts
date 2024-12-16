714f78f0b99df01c0e67af50581b1e3a941dfeb8
SrGobi <alejandrosanjuangallegos@gmail.com>
2024-12-16 15:27:40 +0100

    feat: Eliminar dependencias de chai en las pruebas

--- test/test.ts
+++ test/test.ts
@@ -1,11 +1,2 @@
-import { should, use } from 'chai';
-import spies from 'chai-spies';
-import events from 'chai-events';
-
-use(spies);
-use(events);
-use(should);
-
 import './unit/q.tests';
-
 import './integration/player.tests';
