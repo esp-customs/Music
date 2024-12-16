6e2aa55d3b7e0e4639500c45b0078a69a5ad158f
SrGobi <alejandrosanjuangallegos@gmail.com>
2024-12-16 15:35:09 +0100

    feat: Actualizar bot.ts para usar Client con nuevos intents y mejorar la gestión de eventos

--- demo/bot.ts
+++ demo/bot.ts
@@ -1,33 +1,62 @@
-import { Client } from 'discord.js';
-import { MusicClient, Player, Track } from '../src/music/client';
+import { Client, Events, GatewayIntentBits, Message } from 'discord.js';
+import { MusicClient } from '../src/music/client';
 import { MusicCommands } from './commands';
 
-const bot = new Client();
-const music = new MusicClient();
+const client = new Client({
+  intents: [
+    GatewayIntentBits.AutoModerationExecution,
+    GatewayIntentBits.AutoModerationConfiguration,
+    GatewayIntentBits.DirectMessageReactions,
+    GatewayIntentBits.DirectMessageTyping,
+    GatewayIntentBits.DirectMessages,
+    GatewayIntentBits.GuildEmojisAndStickers,
+    GatewayIntentBits.GuildIntegrations,
+    GatewayIntentBits.GuildInvites,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.GuildMessageReactions,
+    GatewayIntentBits.GuildMessageTyping,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.GuildModeration,
+    GatewayIntentBits.GuildScheduledEvents,
+    GatewayIntentBits.GuildVoiceStates,
+    GatewayIntentBits.GuildWebhooks,
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.MessageContent
+  ]
+});
+const music = new MusicClient();  // Pasamos el bot a MusicClient
 const commands = new MusicCommands(music);
 
-music.on('trackStart', (player: Player, track: Track) => player.textChannel?.send(`**${track.title}** empezado.`));
-music.on('queueEnd', (player: Player) => player.textChannel?.send(`La cola ha terminado.`));
+music.on('trackStart', (player, track) => {
+  player.textChannel?.send(`**${track.title}** ha comenzado.`);
+});
 
-bot.on('message', async (msg) => {
-  if (msg.author.bot) return;
+music.on('queueEnd', (player) => {
+  player.textChannel?.send('La cola ha terminado.');
+});
 
+client.once(Events.ClientReady, readyClient => {
+  console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+});
+
+client.once(Events.MessageCreate, async (msg: Message) => {
   try {
-    if (msg.content.startsWith('.play '))
+    if (msg.content.startsWith('.play ')) {
+      console.log('play');
       await commands.play(msg);
-    if (msg.content.startsWith('.seek '))
+    } else if (msg.content.startsWith('.seek ')) {
       await commands.seek(msg);
-    else if (msg.content === '.stop')
+    } else if (msg.content.startsWith('.stop')) {
+      console.log('stop');
       await commands.stop(msg);
-    else if (msg.content === '.q')
+    } else if (msg.content === '.q') {
       await commands.q(msg);
-    else if (msg.content === '.skip')
+    } else if (msg.content === '.skip') {
       await commands.skip(msg);
+    }
   } catch (error) {
-    await msg.reply(error?.message);
+    console.log(error);
   }
 });
 
-bot.on('ready', () => console.log('¡Bot ha iniciado sesión!'))
-
-bot.login('<el_token_de_tu_bot>');
+client.login('<TOKEN>');
